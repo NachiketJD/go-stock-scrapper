8a9a27657658c5a0fa13da44655fa178a491414f
Nachiket Jayant Deshpande <nachiketdeshpande2004@gmail.com>
2025-07-11 10:25:24 +0530

    NEW

--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/gocolly/colly"
 )
+
+type Stock struct {
+	company, price, change string
+}
 
 func main() {
 	ticker := []string{
@@ -28,10 +33,16 @@ func main() {
 		"CRM",
 		"VZ",
 	}
-	stocks := []stock{}
+	stocks := []Stock{}
 	c := colly.NewCollector()
 
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL)
 	})
+
+	c.OnError(func(_ *colly.Response, err error) {
+		log.Println("Something went wrong: ", err)
+	})
+
+	c.Visit("https://finance.yahoo.com/quote/%5EDJI/components?p=%5EDJI")
 }
